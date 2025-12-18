19eb4bc74461ce9a7efe46c31181f6b3e5afc437
bala-amypo <bala@amypo.in>
2025-12-18 05:12:14 +0000

    User commit

--- src/main/java/com/example/demo/entity/Student.java
+++ src/main/java/com/example/demo/entity/Student.java
@@ -6,36 +6,36 @@ import jakarta.persistence.*;
 //@Table(name="Student")
 public class Student {
     @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
             
-                private Long id;
-                    private String name;
-                        private String email;
+        private Long id;
+        private String name;
+        private String email;
                             
-                                public Student(Long id, String name, String email) {
-                                        this.id = id;
-                                                this.name = name;
-                                                        this.email = email;
-                                                            }
+    public Student(Long id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
 
-                                                                public Long getId() {
-                                                                        return id;
-                                                                            }
-                                                                                public void setId() {
-                                                                                        this.id = id;
-                                                                                            }
+    public Long getId() {
+        return id;
+    }
+    public void setId() {
+        this.id = id;
+    }
                                                                                                 
-                                                                                                    public String getName() {
-                                                                                                            return name;
-                                                                                                                }
-                                                                                                                    public void setName() {
-                                                                                                                            this.name = name;
-                                                                                                                                }
+    public String getName() {
+        return name;
+    }
+    public void setName() {
+        this.name = name;
+    }
 
-                                                                                                                                    public String getEmail() {
-                                                                                                                                            return email;
-                                                                                                                                                }
-                                                                                                                                                    public void setEmail() {
-                                                                                                                                                            this.email = email;
-                                                                                                                                                                }
-                                                                                                                                                                }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail() {
+        this.email = email;
+    }
+}
