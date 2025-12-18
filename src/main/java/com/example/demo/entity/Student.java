84e3bd6672f2c9b365175ebf37aa68a7c5d9fdda
bala-amypo <bala@amypo.in>
2025-12-18 05:05:06 +0000

    User commit

--- src/main/java/com/example/demo/entity/Student.java
+++ src/main/java/com/example/demo/entity/Student.java
@@ -6,26 +6,36 @@ import jakarta.persistence.*;
 //@Table(name="Student")
 public class Student {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
             
-    private String name;
-    private String email;
-    public Student(String name, String email) {
-        this.name = name;
-        this.email = email;
-    }
+                private Long id;
+                    private String name;
+                        private String email;
+                            
+                                public Student(Long id, String name, String email) {
+                                        this.id = id;
+                                                this.name = name;
+                                                        this.email = email;
+                                                            }
 
-    public String getName() {
-        return name;
-    }
-    public void setName() {
-        this.name = name;
-    }
+                                                                public Long getId() {
+                                                                        return id;
+                                                                            }
+                                                                                public void setId() {
+                                                                                        this.id = id;
+                                                                                            }
+                                                                                                
+                                                                                                    public String getName() {
+                                                                                                            return name;
+                                                                                                                }
+                                                                                                                    public void setName() {
+                                                                                                                            this.name = name;
+                                                                                                                                }
 
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail() {
-        this.email = email;
-    }
-}
+                                                                                                                                    public String getEmail() {
+                                                                                                                                            return email;
+                                                                                                                                                }
+                                                                                                                                                    public void setEmail() {
+                                                                                                                                                            this.email = email;
+                                                                                                                                                                }
+                                                                                                                                                                }
