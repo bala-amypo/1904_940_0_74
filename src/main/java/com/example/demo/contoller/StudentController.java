11934c499b66fa754ebb032476b13e8dd31ea8cd
bala-amypo <bala@amypo.in>
2025-12-18 09:32:43 +0000

    User commit

--- src/main/java/com/example/demo/contoller/StudentController.java
+++ src/main/java/com/example/demo/contoller/StudentController.java
@@ -1,24 +1,25 @@
 package com.example.demo.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.http.ResponseEntity;
 import com.example.demo.entity.Student;
 import com.example.demo.service.StudentService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 
 @RestController
-public class StudentController{
+public class StudentController {
     @Autowired
     StudentService studentService;
-    
+
     @PostMapping("/postdata")
-    public Student postdata(@RequestBody Student stu){
-        return studentService.saveStudent(stu);
+    public ResponseEntity<Student> postdata(@Valid @RequestBody Student student){
+        return new  ResponseEntity<>(studentService.saveStudent(student),HttpStatus.CREATED);
     }
+    
+    
 
 }
--- src/main/java/com/example/demo/entity/Student.java
+++ src/main/java/com/example/demo/entity/Student.java
@@ -1,41 +1,63 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.*;
-
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
+import jakarta.persistence.Column;
 @Entity
-//@Table(name="Student")
+// @Table(name = "students")
 public class Student {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-            
-        private Long id;
-        private String name;
-        private String email;
-                            
-    public Student(Long id, String name, String email) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-    }
+    private Long id;
+    @NotBlank(message="Name filed cannot be empty")
+    @Size(min=3,max=20,message="The user name must be min of 3 and max of 20 character")
+    private String name;
+    @Email(message="Invalid email id")
+    @Column(unique=true)
+    private String email;
+
+    // @NotNull
+    // @Size(min,max)
+    // @Min
+    // @Max
+    // @Pattern(reg)
+    // @Postive
 
     public Long getId() {
         return id;
     }
-    public void setId() {
+
+    public void setId(Long id) {
         this.id = id;
     }
-                                                                                                
+
     public String getName() {
         return name;
     }
-    public void setName() {
+
+    public void setName(String name) {
         this.name = name;
     }
 
     public String getEmail() {
         return email;
     }
-    public void setEmail() {
+
+    public void setEmail(String email) {
         this.email = email;
     }
+
+    public Student(String name,String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public Student() {
+    }
+    
+
+    
 }
