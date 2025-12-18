9ee8b0542bae000c20ad13610316b63bf0717ab6
bala-amypo <bala@amypo.in>
2025-12-18 09:42:44 +0000

    User commit

--- src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -19,9 +19,9 @@ public class GlobalExceptionHandler{
         }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Map<String,String>> handleMethod(MethodArgumentNotValidException mex){
+    public ResponseEntity<Map<String,String>> handleMethod(MethodArgumentNotValidException ex){
         
-        Map<String,String> error=new HashMap<>();
+        Map<String,String> errors=new HashMap<>();
 
         ex.getBindingResult().getFieldErrors().forEach(error ->
                 errors.put(error.getField(), error.getDefaultMessage())
